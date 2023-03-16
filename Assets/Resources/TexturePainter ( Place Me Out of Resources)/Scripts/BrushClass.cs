e20bf7156474d0642360acf868be55a620532726
Binglun Wang <bxw135@student.bham.ac.uk>
2023-03-16 10:41:28 +0000

    paint with spray can direction

---
+++ Assets/Resources/TexturePainter ( Place Me Out of Resources)/Scripts/BrushClass.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ubiq.XR;
+using Ubiq.Messaging; // new
+public class BrushClass : MonoBehaviour
+{
+    private NetworkContext context; // new
+    private bool owner; // new
+    private Hand controller;
+    private struct Message
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+
+        public Message(Transform transform)
+        {
+            this.position = transform.position;
+            this.rotation = transform.rotation;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        context = NetworkScene.Register(this);
+    }
+    public void ProcessMessage (ReferenceCountedSceneGraphMessage msg)
+    {
+        // 3. Receive and use transform update messages from remote users
+        // Here we use them to update our current position
+        var data = msg.FromJson<Message>();
+        transform.position = data.position;
+        transform.rotation = data.rotation;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
