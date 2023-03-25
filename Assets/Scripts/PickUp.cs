656027e9c07f95dcfccf270ead688f81261d1c61
郑童琳 <zhengtonglin@eduroam-int-dhcp-97-58-201.ucl.ac.uk>
2023-03-25 20:29:00 +0000

    modified pick up for bucket rotation

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+// using UnityEngine.Events;
 using Ubiq.XR;
 using Ubiq.Messaging; // new
 
@@ -7,6 +8,11 @@ public class PickUp : MonoBehaviour, IGraspable
     private NetworkContext context; // new
     private bool owner; // new
     private Hand controller;
+    public Vector3 hand_bucket_offset;
+
+    // private InputAction rotateAction;
+    private Quaternion previousRotation;
+    // private Quaternion rotated_angle;
 
     // new
     // 1. Define a message format. Let's us know what to expect on send and recv
@@ -47,6 +53,11 @@ public class PickUp : MonoBehaviour, IGraspable
         {
             // 4. Send transform update messages if we are the current 'owner'
             context.SendJson(new Message(transform));
+            GetComponent<Collider>().isTrigger = true;
+        }
+        else
+        {
+            GetComponent<Collider>().isTrigger = false;
         }
     }
 
@@ -54,8 +65,15 @@ public class PickUp : MonoBehaviour, IGraspable
     {
         if (controller)
         {
-            transform.position = controller.transform.position;
-            transform.rotation = controller.transform.rotation;
+            Quaternion currentRotation = controller.transform.rotation;
+            Quaternion rotationChange = currentRotation * Quaternion.Inverse(previousRotation);
+            previousRotation = currentRotation;
+            
+            transform.rotation *= rotationChange;
+
+            transform.position = controller.transform.position + hand_bucket_offset;
+            // transform.rotation = controller.transform.rotation;
+
         }
     }
 
@@ -65,6 +83,10 @@ public class PickUp : MonoBehaviour, IGraspable
         // 5. Define ownership as 'who holds the item currently'
         owner = true; // new
         this.controller = controller;
+
+        previousRotation = controller.transform.rotation;
+
+        GetComponent<Rigidbody>().useGravity = false;
     }
 
     void IGraspable.Release(Hand controller)
@@ -73,7 +95,28 @@ public class PickUp : MonoBehaviour, IGraspable
         // As 5. above, define ownership as 'who holds the item currently'
         owner = false; // new
         this.controller = null;
+        GetComponent<Rigidbody>().useGravity = true;
     }
+
+    // private void OnEnable()
+    // {
+    //     // rotationAction = new InputAction("Rotation",binding:"<XRController}{LeftHand}/rotation");
+    //     // rotationAction.Enable();
+    //     previousRotation = transform.rotation;
+    // }
+    // private void OnDisable()
+    // {
+    //     rotateAction.Disable();
+    // }
+    // private void Update()
+    // {
+    //     Quaternion currentRotation = rotateAction.ReadValue<Quaternion>();
+    //     Quaternion rotationChange = currentRotation * Quaternion.Inverse(previousRotation);
+    //     previousRotation = currentRotation;
+
+    //     transform.rotation *= rotationChange;
+
+    // }
 
      // Note about ownership: 'ownership' is just one way of designing this
      // kind of script. It's sometimes a useful pattern, but has no special
