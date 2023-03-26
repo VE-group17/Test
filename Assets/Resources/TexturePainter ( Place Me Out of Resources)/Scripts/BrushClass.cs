f56de803bb86b50aed2b4e35acc946a99aab6364
Binglun Wang <bxw135@student.bham.ac.uk>
2023-03-26 15:22:42 +0100

    Can do edge detection

--- Assets/Resources/TexturePainter ( Place Me Out of Resources)/Scripts/BrushClass.cs
+++ Assets/Resources/TexturePainter ( Place Me Out of Resources)/Scripts/BrushClass.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using Ubiq.XR;
 using Ubiq.Messaging; // new
+using System.Runtime.ConstrainedExecution;
+
 public class BrushClass : MonoBehaviour
 {
     public Sprite sp1, sp2, spnone;
+    private Sprite edge_sp1, edge_sp2;
+
+    private Sprite cur_sprite;
     private NetworkContext context; // new
     private bool owner; // new
     private Hand controller;
@@ -21,8 +26,37 @@ public class BrushClass : MonoBehaviour
         }
     }
     // Start is called before the first frame update
+
+    Sprite getedge(Sprite cur_sprite)
+    {
+        Texture2D originalTexture = cur_sprite.texture;
+        Texture2D modifiedTexture = new Texture2D(originalTexture.width, originalTexture.height);
+        Sprite edge_sprite = Sprite.Create(modifiedTexture, new Rect(0, 0, originalTexture.width, originalTexture.height), Vector2.zero);
+
+        // Apply edge detection algorithm to original texture and save result in modified texture
+        for (int y = 0; y < originalTexture.height; y++)
+        {
+            for (int x = 0; x < originalTexture.width; x++)
+            {
+                Color modified_pixel = Color.white;
+                Color pixelColour = originalTexture.GetPixel(x, y);
+                if(y > 0)
+                {
+                    modified_pixel += pixelColour - originalTexture.GetPixel(x, y - 1);
+                }
+                modifiedTexture.SetPixel(x, y, modified_pixel);
+                // Apply edge detection algorithm to pixel color and set the modified color in modified texture
+                // ...
+            }
+        }
+        modifiedTexture.Apply();
+        return edge_sprite;
+    }
+
     void Start()
     {
+        edge_sp1 = getedge(sp1);
+        edge_sp2 = getedge(sp2);
         context = NetworkScene.Register(this);
     }
     public void ProcessMessage (ReferenceCountedSceneGraphMessage msg)
@@ -33,20 +67,30 @@ public class BrushClass : MonoBehaviour
         transform.position = data.position;
         transform.rotation = data.rotation;
     }
-    // Update is called once per frame
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.U))
         {
-            GetComponent<SpriteRenderer>().sprite = sp1;
+            cur_sprite = sp1;
         }
         if (Input.GetKeyDown(KeyCode.I))
         {
-            GetComponent<SpriteRenderer>().sprite = sp2;
+            cur_sprite = sp2;
         }
         if (Input.GetKeyDown(KeyCode.O))
         {
-            GetComponent<SpriteRenderer>().sprite = spnone;
+            cur_sprite = spnone;
         }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            cur_sprite = edge_sp1;
+        }
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            cur_sprite = edge_sp2;
+        }
+
+        GetComponent<SpriteRenderer>().sprite = cur_sprite;
     }
 }
