870d530c6c54524f90fff04618ea43ff63eb2abc
Binglun Wang <bxw135@student.bham.ac.uk>
2023-03-28 20:20:00 +0100

    Cancel ray

--- Assets/Raytest.cs
+++ Assets/Raytest.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using Ubiq.Messaging; // new
+using Ubiq.XR;
 
 public class Raytest : MonoBehaviour
 {
@@ -10,9 +12,29 @@ public class Raytest : MonoBehaviour
     public Vector3 collision = Vector3.zero;
     public LayerMask layer;
     private LineRenderer lr;
+    private NetworkContext context; // new
+    private bool owner; // new
 
-    void Start()
+    // new
+    // 1. Define a message format. Let's us know what to expect on send and recv
+    private struct Message
     {
+        public Vector3 position;
+        public Quaternion rotation;
+
+        public Message(Transform transform)
+        {
+            this.position = transform.position;
+            this.rotation = transform.rotation;
+        }
+    }
+
+    private void Start()
+    {
+        // 2. Register the object with the network scene. This provides a
+        // NetworkID for the object and lets it get messages from remote users
+        context = NetworkScene.Register(this);
+
         GameObject myLine = new GameObject();
         Material lineMaterial = new Material(Shader.Find("Transparent/Diffuse"));
         lineMaterial.color = new Color(1f, 1f, 1f, 0.5f);
@@ -23,20 +45,35 @@ public class Raytest : MonoBehaviour
         myLine.transform.position = this.transform.position;
         lr = myLine.GetComponent<LineRenderer>();
         lr.material = lineMaterial;
-        lr.SetWidth(0.1f, 0.1f);
+        lr.SetWidth(0.01f, 0.01f);
         lr.SetPosition(0, this.transform.position);
         lr.SetPosition(1, this.transform.position + this.transform.forward);
+        //myLine.AddComponent<Rigidbody>();
+        //Rigidbody Rb = myLine.GetComponent<Rigidbody>();
+        //Rb.useGravity = false;
+        //Rb.isKinematic = true;
+    }
+
+
+    // new
+    public void ProcessMessage(ReferenceCountedSceneGraphMessage msg)
+    {
+        // 3. Receive and use transform update messages from remote users
+        // Here we use them to update our current position
+        var data = msg.FromJson<Message>();
+        transform.position = data.position;
+        transform.rotation = data.rotation;
+    }
+
+    // new
+    private void FixedUpdate()
+    {
+        // 4. Send transform update messages if we are the current 'owner'
+        context.SendJson(new Message(transform));
 
     }
 
-    // Update is called once per frame
-
-    //void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 0.2f)
-    //{
-    //    //GameObject.Destroy(myLine, duration);
-    //}
-
-    void Update()
+    private void LateUpdate()
     {
         var ray = new Ray(this.transform.position, this.transform.forward);
         RaycastHit hit;
@@ -48,6 +85,4 @@ public class Raytest : MonoBehaviour
         lr.SetPosition(0, this.transform.position);
         lr.SetPosition(1, collision);
     }
-
-    
 }
